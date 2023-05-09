a40a820a0e50041ecf7b12bea897a8c842a9a139
Hosam Elkoulak <hosam.elkoulak@unamur.be>
2023-05-09 17:32:12 +0200

    add data structures for tclsSession, tcpConnection, and tlsContext to the library crate

--- rTcpls/src/lib.rs
+++ rTcpls/src/lib.rs
@@ -1,7 +1,41 @@
-pub mod data {
+
+pub mod common {
+    use std::net::SocketAddr;
+    use std::sync::Arc;
+    use mio::net::TcpStream;
+    use rustls::{ClientConfig, ClientConnection, ServerConfig, ServerConnection, ServerName};
+    use rustls::internal::msgs::handshake::SessionId;
+    use crate::common::TlsContext::TlsClient;
 
 
+    pub struct TcplsSession {
+        tcp_connections: Vec<TcpConnection>,
+        next_connection_id: i32,
+        tls_ctx: TlsContext,
+        local_addresses: Vec<SocketAddr>,
+        next_local_address_id: i8,
+        remote_addresses: Vec<SocketAddr>,
+        next_remote_address_id: i8,
+        next_stream_id: i32,
+        is_server: bool,
+        is_closed: bool,
 
+
+    }
+
+    pub struct TcpConnection{
+        connection_id: i32,
+        streams: Vec<TcpStream>,
+
+    }
+
+    pub struct TlsContext{
+        tls_client: ClientConnection,
+        tls_client_config: ClientConfig,
+        tls_server: ServerConnection,
+        tls_server_config: ServerConfig,
+        server_name: ServerName,
+    }
 
 
 
