69d228678b321b793e3aec32a41cd7b8616b0659
Hosam Elkoulak <hosam.elkoulak@unamur.be>
2023-09-13 11:46:56 +0200

    reduce MAX_FRAGMENT_LEN to allow space for tcpls stream header

--- rustls/src/msgs/fragmenter.rs
+++ rustls/src/msgs/fragmenter.rs
@@ -2,9 +2,11 @@ use crate::enums::ContentType;
 use crate::enums::ProtocolVersion;
 use crate::msgs::message::{BorrowedPlainMessage, PlainMessage};
 use crate::Error;
-pub const MAX_FRAGMENT_LEN: usize = 16384;
+use crate::tcpls::frame::STREAM_FRAME_MAX_OVERHEAD;
+
+pub const MAX_FRAGMENT_LEN: usize = 16384 - STREAM_FRAME_MAX_OVERHEAD;
 pub const PACKET_OVERHEAD: usize = 1 + 2 + 2;
-pub const MAX_FRAGMENT_SIZE: usize = MAX_FRAGMENT_LEN + PACKET_OVERHEAD;
+pub const MAX_FRAGMENT_SIZE: usize = MAX_FRAGMENT_LEN + PACKET_OVERHEAD + STREAM_FRAME_MAX_OVERHEAD;
 
 pub struct MessageFragmenter {
     max_frag: usize,
@@ -44,6 +46,7 @@ impl MessageFragmenter {
                 typ,
                 version,
                 payload: c,
+                stream_header: None,
             })
     }
 
